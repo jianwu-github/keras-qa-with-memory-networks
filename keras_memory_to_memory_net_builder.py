a600344fc6f2c1b78a290725db4c2ddf9774eb5f
Jian Wu <jian.wu@samsung.com>
2018-02-23 16:21:59 -0800

    implementing training end-to-end memory network ...

--- keras_memory_to_memory_net_builder.py
+++ keras_memory_to_memory_net_builder.py
@@ -1,1 +1,68 @@
-from keras.models import Model
+from keras.models import Sequential, Model
+from keras.layers.embeddings import Embedding
+from keras.layers import Input, Activation, Dense, Permute, Dropout, add, dot, concatenate
+from keras.layers import LSTM
+
+class KerasMemToMemNNBuilder:
+
+    def __init__(self, num_of_vocabularies, max_story_len, max_question_len):
+        self._num_of_vocabularies = num_of_vocabularies
+        self._max_story_len = max_story_len
+        self._max_question_len = max_question_len
+
+    def build_mem_to_mem_networks(self, output_dim):
+        # placeholders
+        story = Input((self._max_story_len,))
+        question = Input((self._max_question_len,))
+
+        # encoders
+        # embed the input sequence into a sequence of vectors
+        input_encoder_m = Sequential()
+        input_encoder_m.add(Embedding(input_dim=self._num_of_vocabularies, output_dim=output_dim))
+        input_encoder_m.add(Dropout(0.3))
+        # output: (samples, _max_story_len, embedding_dim)
+
+        # embed the input into a sequence of vectors of size _max_question_len
+        input_encoder_c = Sequential()
+        input_encoder_c.add(Embedding(input_dim=self._num_of_vocabularies, output_dim=self._max_question_len))
+        input_encoder_c.add(Dropout(0.3))
+        # output: (samples, _max_story_len, _max_question_len)
+
+        # embed the question into a sequence of vectors
+        question_encoder = Sequential()
+        question_encoder.add(Embedding(input_dim=self._num_of_vocabularies, output_dim=output_dim, input_length=self._max_question_len))
+        question_encoder.add(Dropout(0.3))
+        # output: (samples, _max_question_len, embedding_dim)
+
+        # encode story and questions (which are indices) to sequences of dense vectors
+        input_encoded_m = input_encoder_m(story)
+        input_encoded_c = input_encoder_c(story)
+        question_encoded = question_encoder(question)
+
+        # compute a 'match' between the first input vector sequence and the question vector sequence
+        # shape: `(samples, _max_story_len, _max_query_len)`
+        match = dot([input_encoded_m, question_encoded], axes=(2, 2))
+        match = Activation('softmax')(match)
+
+        # add the match matrix with the second input vector sequence
+        response = add([match, input_encoded_c])  # (samples, _max_story_len, _max_query_len)
+        response = Permute((2, 1))(response)      # (samples, _max_query_len, _max_story_len)
+
+        # concatenate the match matrix with the question vector sequence
+        answer = concatenate([response, question_encoded])
+
+        # the original paper uses a matrix multiplication for this reduction step, we choose to use a RNN instead.
+        answer = LSTM(32)(answer)  # (samples, 32)
+
+        # one regularization layer -- more would probably be needed.
+        answer = Dropout(0.3)(answer)
+        answer = Dense(self._num_of_vocabularies)(answer)  # (samples, _num_of_vocabularies)
+        # we output a probability distribution over the vocabulary
+        answer = Activation('softmax')(answer)
+
+        # Finally, build the model
+        model = Model([story, question], answer)
+        model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+        return model
+
--- train_memnn_with_babi_10k.py
+++ train_memnn_with_babi_10k.py
@@ -1,8 +1,124 @@
 import argparse
+import pickle
+import re
+
+from contextlib import ExitStack
 
 import numpy as np
 import tensorflow as tf
 from keras import backend as K
+from keras.preprocessing.sequence import pad_sequences
+from functools import reduce
+
+from keras_memory_to_memory_net_builder import KerasMemToMemNNBuilder
+
+_DEFAULT_TRAINING_STORY_FILE = "data/qa1_single-supporting-fact_train.txt"
+_DEFAULT_TESTING_STORY_FILE = "data/qa1_single-supporting-fact_test.txt"
+_DEFAULT_MODEL_DIR = "model"
+
+
+# Helper functions
+def tokenize(sentence):
+    return [x.strip() for x in re.split('(\W+)', sentence) if x.strip()]
+
+
+def parse_stories(lines, only_supporting=False):
+    data = []
+    story = []
+
+    for line in lines:
+        line = line.strip()
+        nid, line = line.split(' ', 1)
+        nid = int(nid)
+
+        if nid == 1:
+            story = []
+
+        if '\t' in line:
+            q, a, supporting = line.split('\t')
+            q = tokenize(q)
+            substory = None
+
+            if only_supporting:
+                # Only select the related substory
+                supporting = map(int, supporting.split())
+                substory = [story[i - 1] for i in supporting]
+            else:
+                # Provide all the substories
+                substory = [x for x in story if x]
+
+            data.append((substory, q, a))
+            story.append('')
+        else:
+            sent = tokenize(line)
+            story.append(sent)
+
+    return data
+
+
+def get_stories(f, only_supporting=False, max_length=None):
+    data = parse_stories(f.readlines(), only_supporting=only_supporting)
+    flatten = lambda data: reduce(lambda x, y: x + y, data)
+    data = [(flatten(story), q, answer) for story, q, answer in data if not max_length or len(flatten(story)) < max_length]
+
+    return data
+
+
+def vectorize_stories(stories, word_idx, max_story_len, max_query_len):
+    inputs, queries, answers = [], [], []
+    for story, query, answer in stories:
+        inputs.append([word_idx[w] for w in story])
+        queries.append([word_idx[w] for w in query])
+        answers.append(word_idx[answer])
+    return (pad_sequences(inputs, maxlen=max_story_len), pad_sequences(queries, maxlen=max_query_len), np.array(answers))
+
+
+def train_memnn_model(training_story_file, testing_story_file, batch_size, epochs):
+    with ExitStack() as stack:
+        training_data = stack.enter_context(open(training_story_file, encoding='utf-8'))
+        testing_data = stack.enter_context(open(testing_story_file, encoding='utf-8'))
+
+        training_stories = get_stories(training_data)
+        testing_stories = get_stories(testing_data)
+
+        # Build Vocabularies
+        vocab = set()
+        for story, q, answer in training_stories + testing_stories:
+            vocab |= set(story + q + [answer])
+
+        vocab = sorted(vocab)
+
+        # Reserve 0 for masking via pad_sequences
+        vocab_size = len(vocab) + 1
+        max_story_len = max(map(len, (x for x, _, _ in training_stories + testing_stories)))
+        max_query_len = max(map(len, (x for _, x, _ in training_stories + testing_stories)))
+
+        word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
+
+        inputs_train, queries_train, answers_train = vectorize_stories(training_stories,
+                                                                       word_idx,
+                                                                       max_story_len,
+                                                                       max_query_len)
+
+        inputs_test, queries_test, answers_test = vectorize_stories(testing_stories,
+                                                                    word_idx,
+                                                                    max_story_len,
+                                                                    max_query_len)
+
+        memnn_builder = KerasMemToMemNNBuilder(vocab_size, max_story_len, max_query_len)
+        model =memnn_builder.build_mem_to_mem_networks(64)
+
+        # train
+        model.fit([inputs_train, queries_train], answers_train,
+                  batch_size=32,
+                  epochs=120,
+                  validation_data=([inputs_test, queries_test], answers_test))
+
+        model_file = _DEFAULT_MODEL_DIR + "/memnn_model.h5"
+        vocab_file = _DEFAULT_MODEL_DIR + "/vocab.pkl"
+
+        model.save(model_file)
+        pickle.dump(vocab, open(vocab_file, "wb"))
 
 
 parser = argparse.ArgumentParser(description="Training End-To-End Memory Networks for Question Answering Tasks")
@@ -11,12 +127,18 @@ parser.add_argument('-bs', dest="batch_size", type=int, default=16, help="Batch 
 
 if __name__ == '__main__':
     parsed_args = parser.parse_args()
+    epochs = parsed_args.epochs
+    batch_size = parsed_args.batch_size
+
+    training_story_file = _DEFAULT_TRAINING_STORY_FILE
+    testing_story_file = _DEFAULT_TESTING_STORY_FILE
 
     tf.logging.set_verbosity(tf.logging.INFO)
 
     tf.reset_default_graph()
 
     # Train the model
+    train_memnn_model(training_story_file, testing_story_file, batch_size, epochs)
 
     # https://github.com/tensorflow/tensorflow/issues/3388
     K.clear_session()
